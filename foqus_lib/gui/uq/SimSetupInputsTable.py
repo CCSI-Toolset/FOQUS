f50668f53d69a57b16fa63be93e6c68e423f7141
U-THE-LAB\ou3 <ou3@CRYSTALCREEK.the-lab.llnl.gov>
2018-02-15 15:30:08 -0800

    Lay the groundwork for refactor of InputPriorTable

---
+++ foqus_lib/gui/uq/SimSetupInputsTable.py
@@ -0,0 +1,7 @@
+__author__ = 'ou3'
+
+from foqus_lib.gui.common.InputPriorTable import InputPriorTable
+
+class SimSetupInputsTable(InputPriorTable):
+    def __init__(self, parent = None):
+        super(SimSetupInputsTable, self).__init__(parent)

2114e775e690a11d10158c21ab3b59fe3b52733d
boverhof <jrboverhof@lbl.gov>
2018-06-12 04:01:30 +0000

    windows service wrapper

---
+++ cloud/aws/foqus_service.py
@@ -0,0 +1,40 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import time
+import logging
+import logging.config
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(filename='TestService.log',level=logging.DEBUG)
+_log = logging.getLogger()
+_log.debug('Loading')
+
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "TestService"
+    _svc_display_name_ = "Test Service"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        for i in range(1000):
+            print "HI: %d" %i
+            _log.info('Running: %d', i)
+            time.sleep(1)
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)

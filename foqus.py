5e81e7e06cf5e705fe101edaff44616942acb522
fsoepyan <30811140+fsoepyan@users.noreply.github.com>
2018-11-07 17:47:15 -0500

    Updated foqus.py: Removed QtXml from list of imports
    This should take care of Issue #314.

--- foqus.py
+++ foqus.py
@@ -49,7 +49,7 @@ def guiImport():
         from PyQt5.QtWidgets import QApplication, QSplashScreen, QMessageBox, \
             QFileDialog
         from PyQt5.QtGui import QPixmap, QPainter
-        from PyQt5 import QtSvg, QtXml
+        from PyQt5 import QtSvg
         #import foqus_lib.gui.icons_rc
         # This import has to be done before QApplicationCore instance is created, but also only works for later versions
         # of PyQt5. This is also only necesary because QWebView was depecrated in later versions of PyQt5

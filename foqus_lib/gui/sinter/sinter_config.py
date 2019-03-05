1a5d0b9eab0a0ea2ade98d4bfe5fa91c3f01dbcf
John Eslick <jeslick@AD.SCILAN.PGH>
2019-03-05 16:28:10 -0500

    Tables of input and output a bit more functional

--- foqus_lib/gui/sinter/sinter_config.py
+++ foqus_lib/gui/sinter/sinter_config.py
@@ -3,7 +3,7 @@ import json
 import logging
 from PyQt5 import uic
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QFileDialog, QDialog, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QDialog, QMessageBox, QAbstractItemView
 import hashlib
 
 _log = logging.getLogger(__name__)
@@ -22,8 +22,33 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         self.actionLoad.triggered.connect(self._load_dialog)
         self.set_working_dir.clicked.connect(self._browse_for_working_dir)
         self.digest_calculate.clicked.connect(self._calc_model_digest)
-
+        self.add_input.clicked.connect(self._add_input)
+        self.add_output.clicked.connect(self._add_output)
+        self.delete_input.clicked.connect(self._del_input)
+        self.delete_output.clicked.connect(self._del_output)
+        self.outputs_table.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.inputs_table.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.outputs_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.inputs_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.show()
+
+    def _add_input(self):
+        self.inputs_table.setRowCount(self.inputs_table.rowCount() + 1)
+
+    def _add_output(self):
+        self.outputs_table.setRowCount(self.outputs_table.rowCount() + 1)
+
+    def _del_input(self):
+        if len(self.inputs_table.selectedIndexes()) == 0:
+            return
+        row = self.inputs_table.selectedIndexes()[0].row()
+        self.inputs_table.removeRow(row)
+
+    def _del_output(self):
+        if len(self.outputs_table.selectedIndexes()) == 0:
+            return
+        row = self.outputs_table.selectedIndexes()[0].row()
+        self.outputs_table.removeRow(row)
 
     def _calc_model_digest(self):
         file = self.model_file.text()
@@ -51,7 +76,6 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
 
         # Currently doesn't load these because you can't change them
         # filetype, filetype-version, and SignatureMethodAlgorithm
-
         index = self.application_name.findText(d["application"].get("name", ""))
         if index >= 0:
             self.application_name.setCurrentIndex(index)
@@ -64,7 +88,17 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         if "model" in d:
             self.model_file.setText(d["model"].get("file", ""))
             self.model_digest_value.setText(d["model"].get("DigestValue",""))
-
+        for row in range(self.settings_table.rowCount()):
+            if not "settings" in d:
+                break
+            sname = self.settings_table.verticalHeaderItem(row).text()
+            if sname in d["settings"]:
+                self.settings_table.item(row, 0).setText(
+                    d["settings"][sname]["type"])
+                self.settings_table.item(row, 1).setText(
+                    d["settings"][sname]["default"])
+                self.settings_table.item(row, 2).setText(
+                    d["settings"][sname]["description"])
 
     def _save(self, file="test.json"):
         d = {}
@@ -82,9 +116,38 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         d["model"] = {}
         d["model"]["file"] = self.model_file.text()
         d["model"]["DigestValue"] = self.model_digest_value.text()
-
         d["settings"] = {}
+        for row in range(self.settings_table.rowCount()):
+            sname = self.settings_table.verticalHeaderItem(row).text()
+            d["settings"][sname] = {}
+            d["settings"][sname]["type"] = \
+                self.settings_table.item(row, 0).text()
+            d["settings"][sname]["default"] = \
+                self.settings_table.item(row, 1).text()
+            d["settings"][sname]["description"] = \
+                self.settings_table.item(row, 2).text()
         d["inputs"] = {}
+        for i in range(self.inputs_table.rowCount()):
+            name = self.inputs_table.item(i, 0).text()
+            d["inputs"][name] = {}
+            d["inputs"][name]["path"] = [self.inputs_table.item(i, 1).text()]
+            d["inputs"][name]["type"] = self.inputs_table.item(i, 2).text()
+            if d["inputs"][name]["type"] == "float":
+                d["inputs"][name]["type"] = "double"
+            d["inputs"][name]["default"] = self.inputs_table.item(i, 3).text()
+            d["inputs"][name]["max"] = self.inputs_table.item(i, 4).text()
+            d["inputs"][name]["min"] = self.inputs_table.item(i, 5).text()
+            d["inputs"][name]["units"] = self.inputs_table.item(i, 6).text()
+            d["inputs"][name]["description"] = self.inputs_table.item(i, 7).text()
+            if d["inputs"][name]["type"] == "int":
+                d["inputs"][name]["default"] = int(d["inputs"][name]["default"])
+                d["inputs"][name]["min"] = int(d["inputs"][name]["min"])
+                d["inputs"][name]["max"] = int(d["inputs"][name]["max"])
+            elif d["inputs"][name]["type"] == "double":
+                d["inputs"][name]["default"] = float(d["inputs"][name]["default"])
+                d["inputs"][name]["min"] = float(d["inputs"][name]["min"])
+                d["inputs"][name]["max"] = float(d["inputs"][name]["max"])
+
         d["outputs"] = {}
         with open(file, "w") as fp:
             json.dump(d, fp, indent=2)

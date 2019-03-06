284ff853afecec971b5341844a30973343136275
John Eslick <jeslick@AD.SCILAN.PGH>
2019-03-06 10:02:57 -0500

    Finishing up sinter config utility

--- foqus_lib/gui/sinter/sinter_config.py
+++ foqus_lib/gui/sinter/sinter_config.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QFileDialog, QDialog, QMessageBox,
                              QAbstractItemView, QTableWidgetItem)
 import hashlib
+import foqus_lib.gui.helpers.guiHelpers as gh
 
 _log = logging.getLogger(__name__)
 
@@ -25,14 +26,17 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         self.model_browse.clicked.connect(self._model_browse)
         self.digest_calculate.clicked.connect(self._calc_model_digest)
         self.add_input_file.clicked.connect(self._add_input_file)
+        self.del_input_file.clicked.connect(self._del_input_file)
         self.add_input.clicked.connect(self._add_input)
         self.add_output.clicked.connect(self._add_output)
         self.delete_input.clicked.connect(self._del_input)
         self.delete_output.clicked.connect(self._del_output)
         self.outputs_table.setSelectionMode(QAbstractItemView.SingleSelection)
         self.inputs_table.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.input_files_table.setSelectionMode(QAbstractItemView.SingleSelection)
         self.outputs_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.inputs_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.input_files_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.show()
 
     def _add_input_file(self):
@@ -42,15 +46,26 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
             file = os.path.relpath(res)
             row = self.input_files_table.rowCount()
             self.input_files_table.setRowCount(row + 1)
-            item = QTableWidgetItem(file)
-            self.input_files_table.setItem(row, 0, item)
+            gh.setTableItem(self.input_files_table, row, 0, file)
             with open(file, "rb") as fp:
                 h = hashlib.sha1(fp.read()).hexdigest()
-            item = QTableWidgetItem(h)
-            self.input_files_table.setItem(row, 1, item)
-            item = QTableWidgetItem("sha1")
-            self.input_files_table.setItem(row, 2, item)
+            gh.setTableItem(self.input_files_table, row, 1, h)
+            gh.setTableItem(self.input_files_table, row, 2, "sha1",
+                pullDown=["sha1"])
             self.input_files_table.resizeColumnsToContents()
+
+    def _add_blank_input_file(self):
+        row = self.input_files_table.rowCount()
+        self.input_files_table.setRowCount(row + 1)
+        gh.setTableItem(self.input_files_table, row, 0, "")
+        gh.setTableItem(self.input_files_table, row, 1, "")
+        gh.setTableItem(self.input_files_table, row, 2, "sha1", pullDown=["sha1"])
+
+    def _del_input_file(self):
+        if len(self.input_files_table.selectedIndexes()) == 0:
+            return
+        row = self.input_files_table.selectedIndexes()[0].row()
+        self.input_files_table.removeRow(row)
 
     def _model_browse(self):
         dialog = QFileDialog(self, 'Model File', directory=os.getcwd())
@@ -60,10 +75,24 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
             self.inputs_table.setRowCount(self.inputs_table.rowCount() + 1)
 
     def _add_input(self):
-        self.inputs_table.setRowCount(self.inputs_table.rowCount() + 1)
+        row = self.inputs_table.rowCount()
+        self.inputs_table.setRowCount(row + 1)
+        for i in range(8):
+            if i == 2:
+                gh.setTableItem(self.inputs_table, row, i, "double",
+                    pullDown=["double", "int", "string"])
+            else:
+                gh.setTableItem(self.inputs_table, row, i, "")
 
     def _add_output(self):
-        self.outputs_table.setRowCount(self.outputs_table.rowCount() + 1)
+        row = self.outputs_table.rowCount()
+        self.outputs_table.setRowCount(row + 1)
+        for i in range(6):
+            if i == 2:
+                gh.setTableItem(self.outputs_table, row, i, "double",
+                    pullDown=["double", "int", "string"])
+            else:
+                gh.setTableItem(self.outputs_table, row, i, "")
 
     def _del_input(self):
         if len(self.inputs_table.selectedIndexes()) == 0:
@@ -86,12 +115,21 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
             _log.exception("Failed to has model file {}".format(file))
 
     def _save_dialog(self):
-        self._save()
-        return True
+        file = "{}{}".format(self.model_file.text().split(".")[0], ".json")
+        dialog = QFileDialog(self, 'Save Config', directory=os.getcwd())
+        dialog.setAcceptMode(QFileDialog.AcceptSave)
+        dialog.selectFile(file)
+        if dialog.exec_() == QDialog.Accepted:
+            self._save(file=dialog.selectedFiles()[0])
+            return True
+        return False
 
     def _load_dialog(self):
-        self._load()
-        return True
+        dialog = QFileDialog(self, 'Load Config', directory=os.getcwd())
+        if dialog.exec_() == QDialog.Accepted:
+            self._load(file=dialog.selectedFiles()[0])
+            return True
+        return False
 
     def _load(self, file="test.json"):
         try:
@@ -115,17 +153,53 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         if "model" in d:
             self.model_file.setText(d["model"].get("file", ""))
             self.model_digest_value.setText(d["model"].get("DigestValue",""))
-        for row in range(self.settings_table.rowCount()):
+        tbl = self.settings_table
+        for row in range(tbl.rowCount()):
             if not "settings" in d:
                 break
-            sname = self.settings_table.verticalHeaderItem(row).text()
+            sname = tbl.verticalHeaderItem(row).text()
             if sname in d["settings"]:
-                self.settings_table.item(row, 0).setText(
-                    d["settings"][sname]["type"])
-                self.settings_table.item(row, 1).setText(
-                    d["settings"][sname]["default"])
-                self.settings_table.item(row, 2).setText(
-                    d["settings"][sname]["description"])
+                gh.setCellText(tbl, row, 0, d["settings"][sname]["type"])
+                gh.setCellText(tbl, row, 1, d["settings"][sname]["default"])
+                gh.setCellText(tbl, row, 2, d["settings"][sname]["description"])
+        files = d.get("input-files", [])
+        tbl = self.input_files_table
+        tbl.setRowCount(0)
+        for row, file in enumerate(files):
+            self._add_blank_input_file()
+            gh.setCellText(tbl, row, 0, file["file"])
+            gh.setCellText(tbl, row, 1, file["DigestValue"])
+            gh.cellPulldownSetText(tbl, row, 2, file["SignatureMethodAlgorithm"])
+        tbl = self.inputs_table
+        tbl.setRowCount(0)
+        for x, xd in d.get("inputs", {}).items():
+            row = tbl.rowCount()
+            self._add_input()
+            gh.setCellText(tbl, row, 0, x)
+            gh.setCellText(tbl, row, 1, xd.get("path", [""])[0])
+            gh.cellPulldownSetText(tbl, row, 2, xd.get("type", "double"))
+            gh.setCellText(tbl, row, 3, xd.get("default", ""))
+            gh.setCellText(tbl, row, 4, xd.get("max", ""))
+            gh.setCellText(tbl, row, 5, xd.get("min", ""))
+            units = xd.get("units", "")
+            if units is None:
+                units = ""
+            gh.setCellText(tbl, row, 6, units)
+            gh.setCellText(tbl, row, 7, xd.get("description", ""))
+        tbl = self.outputs_table
+        tbl.setRowCount(0)
+        for x, xd in  d.get("outputs", {}).items():
+            row = tbl.rowCount()
+            self._add_output()
+            gh.setCellText(tbl, row, 0, x)
+            gh.setCellText(tbl, row, 1, xd.get("path", [""])[0])
+            gh.cellPulldownSetText(tbl, row, 2, xd.get("type", "double"))
+            gh.setCellText(tbl, row, 3, xd.get("default", ""))
+            units = xd.get("units", "")
+            if units is None:
+                units = ""
+            gh.setCellText(tbl, row, 4,units)
+            gh.setCellText(tbl, row, 5, xd.get("description", ""))
 
     def _save(self, file="test.json"):
         d = {}
@@ -144,66 +218,86 @@ class SinterConfigMainWindow(_sinterConfigUI, _sinterConfig):
         d["model"]["file"] = self.model_file.text()
         d["model"]["DigestValue"] = self.model_digest_value.text()
         d["input-files"] = []
-        for row in range(self.input_files_table.rowCount()):
+        tbl = self.input_files_table
+        for row in range(tbl.rowCount()):
             d2 = {}
-            d2["file"] = self.input_files_table.item(row, 0).text()
-            d2["DigestValue"] = self.input_files_table.item(row, 1).text()
-            d2["SignatureMethodAlgorithm"] = \
-                self.input_files_table.item(row, 2).text()
+            d2["file"] = gh.getCellText(tbl, row, 0)
+            d2["DigestValue"] = gh.getCellText(tbl, row, 1)
+            d2["SignatureMethodAlgorithm"] = gh.getCellText(tbl, row, 2)
             d["input-files"].append(d2)
         d["settings"] = {}
-        for row in range(self.settings_table.rowCount()):
-            sname = self.settings_table.verticalHeaderItem(row).text()
+        tbl = self.settings_table
+        for row in range(tbl.rowCount()):
+            sname = tbl.verticalHeaderItem(row).text()
             d["settings"][sname] = {}
-            d["settings"][sname]["type"] = \
-                self.settings_table.item(row, 0).text()
-            d["settings"][sname]["default"] = \
-                self.settings_table.item(row, 1).text()
-            d["settings"][sname]["description"] = \
-                self.settings_table.item(row, 2).text()
+            d["settings"][sname]["type"] = gh.getCellText(tbl, row, 0)
+            d["settings"][sname]["default"] = gh.getCellText(tbl, row, 1)
+            d["settings"][sname]["description"] = gh.getCellText(tbl, row, 2)
         d["inputs"] = {}
-        for i in range(self.inputs_table.rowCount()):
-            if self.inputs_table.item(i, 0) is None:
-                continue
-            name = self.inputs_table.item(i, 0).text()
+        tbl = self.inputs_table
+        for row in range(tbl.rowCount()):
+            name = gh.getCellText(tbl, row, 0)
             d["inputs"][name] = {}
-            d["inputs"][name]["path"] = [self.inputs_table.item(i, 1).text()]
-            d["inputs"][name]["type"] = self.inputs_table.item(i, 2).text()
-            if d["inputs"][name]["type"] == "float":
-                d["inputs"][name]["type"] = "double"
-            d["inputs"][name]["default"] = self.inputs_table.item(i, 3).text()
-            d["inputs"][name]["max"] = self.inputs_table.item(i, 4).text()
-            d["inputs"][name]["min"] = self.inputs_table.item(i, 5).text()
-            d["inputs"][name]["units"] = self.inputs_table.item(i, 6).text()
-            d["inputs"][name]["description"] = self.inputs_table.item(i, 7).text()
+            d["inputs"][name]["path"] = [ gh.getCellText(tbl, row, 1)]
+            d["inputs"][name]["type"] =  gh.getCellText(tbl, row, 2)
+            d["inputs"][name]["default"] =  gh.getCellText(tbl, row, 3)
+            d["inputs"][name]["max"] =  gh.getCellText(tbl, row, 4)
+            d["inputs"][name]["min"] =  gh.getCellText(tbl, row, 5)
+            d["inputs"][name]["units"] =  gh.getCellText(tbl, row, 6)
+            d["inputs"][name]["description"] =  gh.getCellText(tbl, row, 7)
+            if d["inputs"][name]["units"] == "":
+                d["inputs"][name]["units"] = None
+            if d["inputs"][name]["type"] in ["int", "double"]:
+                if d["inputs"][name]["default"] == "":
+                    d["inputs"][name]["default"] = 0
+                if d["inputs"][name]["min"] == "":
+                    d["inputs"][name]["min"] = 0
+                if d["inputs"][name]["max"] == "":
+                    d["inputs"][name]["max"] = 0
             if d["inputs"][name]["type"] == "int":
-                d["inputs"][name]["default"] = int(d["inputs"][name]["default"])
-                d["inputs"][name]["min"] = int(d["inputs"][name]["min"])
-                d["inputs"][name]["max"] = int(d["inputs"][name]["max"])
+                try:
+                    d["inputs"][name]["default"] = int(d["inputs"][name]["default"])
+                    d["inputs"][name]["min"] = int(d["inputs"][name]["min"])
+                    d["inputs"][name]["max"] = int(d["inputs"][name]["max"])
+                except:
+                    _log.exception("Error converting to int")
             elif d["inputs"][name]["type"] == "double":
-                d["inputs"][name]["default"] = float(d["inputs"][name]["default"])
-                d["inputs"][name]["min"] = float(d["inputs"][name]["min"])
-                d["inputs"][name]["max"] = float(d["inputs"][name]["max"])
+                try:
+                    d["inputs"][name]["default"] = float(d["inputs"][name]["default"])
+                    d["inputs"][name]["min"] = float(d["inputs"][name]["min"])
+                    d["inputs"][name]["max"] = float(d["inputs"][name]["max"])
+                except:
+                    _log.exception("Error converting to double")
+            elif d["inputs"][name]["type"] == "string":
+                del d["inputs"][name]["min"]
+                del d["inputs"][name]["max"]
         d["outputs"] = {}
-        for i in range(self.outputs_table.rowCount()):
-            if self.inputs_table.item(i, 0) is None:
-                continue
-            name = self.outputs_table.item(i, 0).text()
+        tbl = self.outputs_table
+        for row in range(tbl.rowCount()):
+            name = gh.getCellText(tbl, row, 0)
             d["outputs"][name] = {}
-            d["outputs"][name]["path"] = [self.outputs_table.item(i, 1).text()]
-            d["outputs"][name]["type"] = self.outputs_table.item(i, 2).text()
-            if d["outputs"][name]["type"] == "float":
-                d["outputs"][name]["type"] = "double"
-            d["outputs"][name]["default"] = self.outputs_table.item(i, 3).text()
-            d["outputs"][name]["units"] = self.inputs_table.item(i, 5).text()
-            d["outputs"][name]["description"] = self.inputs_table.item(i, 5).text()
+            d["outputs"][name]["path"] = [gh.getCellText(tbl, row, 1)]
+            d["outputs"][name]["type"] = gh.getCellText(tbl, row, 2)
+            d["outputs"][name]["default"] = gh.getCellText(tbl, row, 3)
+            d["outputs"][name]["units"] = gh.getCellText(tbl, row, 4)
+            d["outputs"][name]["description"] = gh.getCellText(tbl, row, 5)
+            if d["outputs"][name]["units"] == "":
+                d["outputs"][name]["units"] = None
+            if d["outputs"][name]["type"] in ["int", "double"]:
+                if d["outputs"][name]["default"] == "":
+                    d["outputs"][name]["default"] = 0
             if d["outputs"][name]["type"] == "int":
-                d["outputs"][name]["default"] = int(d["outputs"][name]["default"])
+                try:
+                    d["outputs"][name]["default"] = int(d["outputs"][name]["default"])
+                except:
+                    _log.exception("Error converting to int")
             elif d["outputs"][name]["type"] == "double":
-                d["outputs"][name]["default"] = float(d["outputs"][name]["default"])
+                try:
+                    d["outputs"][name]["default"] = float(d["outputs"][name]["default"])
+                except:
+                    _log.exception("Error converting to double")
         with open(file, "w") as fp:
             json.dump(d, fp, indent=2)
-
 
     def _browse_for_working_dir(self):
         dialog = QFileDialog(self, 'Working Directory', directory=os.getcwd())

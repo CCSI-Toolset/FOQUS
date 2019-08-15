998a5f1321708b123a5d0ed0d5dca09aa6c0b05b
Joshua Boverhof <boverhof@joshuas-mbp.lan>
2019-08-15 16:09:41 -0700

    Replace console.log with "log"

--- cloud/aws/node/get-simulation-input-file/index.js
+++ cloud/aws/node/get-simulation-input-file/index.js
@@ -8,8 +8,8 @@
  */
 'use strict';
 'use AWS.S3'
-console.log('Loading function');
 const AWS = require('aws-sdk');
+const log = require("debug")("get-simulation-input-file");
 //const s3 = require('s3');
 const fs = require('fs');
 const dirPath = "./tmp";
@@ -19,10 +19,10 @@ const s3_bucket_name = process.env.SIMULATION_BUCKET_NAME;
 
 // PATH: /simulation/OUU/input/configuration
 exports.handler = function(event, context, callback) {
-  console.log(`Running index.handler: "${event.httpMethod}"`);
-  console.log("event: " + JSON.stringify(event));
-  console.log("query: " + event.queryStringParameters);
-  console.log('==================================');
+  log(`Running index.handler: "${event.httpMethod}"`);
+  log("event: " + JSON.stringify(event));
+  log("query: " + event.queryStringParameters);
+  log('==================================');
   const done = (err, res) => callback(null, {
       statusCode: err ? '400' : '200',
       body: err ? err.message : JSON.stringify(res),
@@ -66,20 +66,27 @@ exports.handler = function(event, context, callback) {
     var params = {
       Bucket: s3_bucket_name
     };
+    log(`KEY: ${key}`);
     if (key == "configuration") {
       params.Prefix = user_name + "/" + sim_name + "/";
     }
     else {
       params.Key = user_name + "/" + sim_name + "/" + key;
-      console.log("FILE RETRIEVE: " + params.Key);
+      log("FILE RETRIEVE: " + params.Key);
       client.getObject(params, function(err, data) {
         /* FILE RETRIEVE: anonymous/OUU/BFB/BFB_v11_FBS_01_26_2018.acmf
         *  body size is too long
         *  Getting this error when file is > 6MB
         *
         **/
-         if (err)
-           console.log(err, err.stack); // an error occurred
+         if (err) {
+           log(err, err.stack); // an error occurred
+           if (err.code == 'NoSuchKey') {
+             callback(null, {statusCode:'404', body: `No such file: ${params.Key}`,
+               headers: {'Access-Control-Allow-Origin': '*','Content-Type': 'text/plain'}
+             });
+           }
+         }
          else
            callback(null, {statusCode:'200', body: data.Body.toString('utf-8'),
              headers: {'Access-Control-Allow-Origin': '*','Content-Type': 'application/octet-stream'}
@@ -89,11 +96,10 @@ exports.handler = function(event, context, callback) {
     }
     // configuration
     // Search for *.foqus or sinter.json
-    console.log("PREFIX: " + params.Prefix);
+    log("S3 List: params: " + JSON.stringify(params));
     client.listObjects(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
+      if (err) log(err, err.stack); // an error occurred
       else {
-        console.log("PATH: " + event.path);
         //var name = event.path.split('/').pop();
         // FIND ALL FLOWSHEETS
         var params =  {
@@ -105,7 +111,7 @@ exports.handler = function(event, context, callback) {
             if (k.endsWith("/session.foqus")) {
               var a = k.split('/');
               a.pop();
-              console.log("FILE: " + k);
+              log("FILE: " + k);
               if (a.pop() != sim_name) continue;
               params.Key = data.Contents[index].Key;
               break;
@@ -113,17 +119,17 @@ exports.handler = function(event, context, callback) {
             if (k.endsWith("-sinter.json")) {
               var a = k.split('/');
               a.pop();
-              console.log("FILE: " + k);
+              log("FILE: " + k);
               if (a.pop() != sim_name) continue;
               params.Key = data.Contents[index].Key;
               break;
             }
         }
         if (params.Key != null) {
-          console.log("CONFIG RETRIEVE: " + params.Key);
+          log("CONFIG RETRIEVE: " + params.Key);
           client.getObject(params, function(err, data) {
              if (err)
-               console.log(err, err.stack); // an error occurred
+               log(err, err.stack); // an error occurred
              else
                callback(null, {statusCode:'200', body: data.Body.toString('utf-8'),
                  headers: {'Access-Control-Allow-Origin': '*','Content-Type': 'application/octet-stream'}
@@ -140,6 +146,6 @@ exports.handler = function(event, context, callback) {
   else {
           done(new Error(`Unsupported method "${event.httpMethod}"`));
   }
-  console.log('==================================');
-  console.log('Stopping index.handler');
+  log('==================================');
+  log('Stopping index.handler');
 };

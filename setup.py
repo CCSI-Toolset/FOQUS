85f56a46d94098b69882b40e3822688611c8e7e4
Keith Beattie <KSBeattie@lbl.gov>
2019-09-24 15:20:19 -0700

    Setting version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import shutil
 # default_version is the version if "git describe --tags" falls through
 # Addtional package info is set in foqus_lib/version/version.template.
 # The version module, just makes it a bit easier for FOQUS to pull package info
-default_version = "3.0.0"
+default_version = "3.3.0"
 
 try:
     version=subprocess.check_output(

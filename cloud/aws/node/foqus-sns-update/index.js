b59ff29ee863f54141ac599ee9133a424f75c794
Joshua Boverhof <jrboverhof@lbl.gov>
2019-10-02 14:54:44 -0700

    Foqus gateway v2 (#588)
    * API Gateway "kill" session just sends SNS messages to update topic
    
    * Check DynamoDB table if job is terminated, if job has Finished key skip it.
    
    * DynamoDB table use only "Finished" timestamp and "State", rather than separate "error", "success" columns
    
    * Added better descriptions of lambda funcs
    
    * Set consumer GUID to 00000000-0000-0000-0000-000000000000
    
    * Add TTL to job items in DynamoDB
    
    * Publish SNS event `session.kill.{session_id}`
    
    * Separate event handlers for session, job, consumer resources and events like kill, etc.
    
    * Reorganized text
    
    * Add /session/{uuid}/kill hook
    
    * add success to legal job states
    
    * Use "Finished" for all final state timestamps and State for the staus
    
    * Powershell directions for setting path for foqus_service
    
    * Updates to make tests work across functions
    
    * Removed "async" in handler func that messed up promises.
    
    * Change console.log to the debug log
    
    * Delete S3Objects once submitted
    
    * Return last result page number.  0 if none available
    
    * Content-type returned json
    
    * Add check for terminate in DynamoDB table
    
    * make specific Finish state checked terminate
    
    * Change to key search patterns for foqus file

--- cloud/aws/node/foqus-sns-update/index.js
+++ cloud/aws/node/foqus-sns-update/index.js
@@ -12,22 +12,70 @@
 'use AWS.S3'
 'use AWS.DynamoDB'
 'use uuid'
+const assert = require('assert');
 const log = require("debug")("foqus-sns-update")
 const AWS = require('aws-sdk');
 const util = require('util');
 const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-const s3_bucket_name = process.env.SESSION_BUCKET_NAME;
-//const update_topic_name = process.env.FOQUS_UPDATE_TOPIC;
-const log_topic_name = process.env.FOQUS_LOG_TOPIC_NAME;
 const tablename = process.env.FOQUS_DYNAMO_TABLE_NAME;
+const s3_bucket_name = process.env.SESSION_BUCKET_NAME;
+const log_topic_name = process.env.FOQUS_LOG_TOPIC_NAME;
+
+// TODO: Check Table and skip any items where Finished is already set
+var process_session_event_terminate = function(ts, session_id, callback) {
+    log(`"process_session_event_terminate(${session_id})"`);
+    function dynamoUpdateItems(obj) {
+        log(`dynamoUpdateItems:  count=${obj.Count}`);
+        log(`dynamoUpdateItems:  ${JSON.stringify(obj)}`);
+        if (obj.Count == 0) {
+          log("Session contains no jobs");
+          return;
+        }
+        var params = {
+          TableName: tablename,
+          UpdateExpression: 'SET #f=:f,  #t=:t, #s=:s',
+          ExpressionAttributeNames: {"#s": 'state', '#f' : 'finished', '#t':'TTL'},
+          ExpressionAttributeValues: {':s': 'terminate', ':f': ts, ':t':Math.floor(Date.now()/1000 + 60*60*12)}
+        };
+        var item = null;
+        var promises = [];
+        for(var i=0; i<obj.Count; i++) {
+          log(`item:  ${JSON.stringify(obj.Items[i])}`);
+          item = {State:"terminate"};
+          params.Key = { "Id": obj.Items[i].Id, "Type":"Job" };
+          log(`item:  ${JSON.stringify(params)}`);
+          var response = dynamodb.update(params);
+          promises.push(response.promise());
+        }
+        Promise.all(promises).then(handleDone).catch(handleError);
+    };
+    function handleError(error) {
+      log(`handleError ${error}`);
+      callback(error, "ERROR");
+    };
+    function handleDone() {
+        callback(null, "SUCCESS");
+    }
+    var params = {
+        TableName: tablename,
+        KeyConditionExpression: '#T = :job',
+        ExpressionAttributeNames: {"#T":"Type"},
+        ExpressionAttributeValues: { ":job":"Job", ":sessionid":session_id},
+        FilterExpression: 'contains (SessionId, :sessionid)'
+    };
+    var promise = dynamodb.query(params).promise();
+    promise.then(dynamoUpdateItems)
+      .then(handleDone)
+      .catch(handleError);
+};
 
 //
 // process_job_event: Adds timestamp to the status field, or if output is
 //   specified it adds an output field to item.
 //
-var process_job_event = function(ts, user_name, message, callback) {
+var process_job_event_output = function(ts, user_name, message, callback) {
     // ts -- "Timestamp": "2018-05-10T01:47:26.794Z",
-    log('process_job_event(username= ' + user_name + ')');
+    log('process_job_event_output(username= ' + user_name + ')');
     const s3 = new AWS.S3();
     const e = message['event'];
     const status = message['status'];
@@ -36,55 +84,30 @@ var process_job_event = function(ts, user_name, message, callback) {
     const consumer = message['consumer'];
     const session = message['sessionid']
     const error_message = message['message'];
-
+    var output = message['value'];
+    assert.strictEqual(e, "output");
     var params = {
         TableName:tablename,
         Key:{
             "Id": job,
             "Type":"Job"
         },
-        UpdateExpression: "set #w=:s, ConsumerId=:c, #u=:u",
+        UpdateExpression: "set #w = :o, #t = :t",
         ExpressionAttributeValues:{
-            ":s":ts,
-            ":c":consumer,
-            ":u":user_name
+            ":o":output,
+            ":t":Math.floor(Date.now()/1000 + 60*60*24)
         },
         ExpressionAttributeNames:{
-            "#w":status,
-            "#u":"User"
+            "#w":"output",
+            "#t":"TTL"
         },
         ReturnValues:"UPDATED_NEW"
     };
-    // TODO:  ADD EVENT for Message..
-    if (e == 'output') {
-        //var output = JSON.stringify(message['value']['output']);
-        var output = message['value'];
-
-        //params.UpdateExpression = "set output=:o";
-        //params.ExpressionAttributeValues = {":o":message['value']};
-        params = {
-            TableName:tablename,
-            Key:{
-                "Id": job,
-                "Type":"Job"
-            },
-            UpdateExpression: "set #w = :o",
-            ExpressionAttributeValues:{
-                ":o":output
-            },
-            ExpressionAttributeNames:{
-                "#w":"output"
-            },
-            ReturnValues:"UPDATED_NEW"
-        };
-    }
-
     log(JSON.stringify(params));
     publish_to_log_topic(e);
-
     function getDynamoJob() {
         // Job is success, put in S3
-        log("Get Job Description from dynamodb");
+        log("Output: Get Job Description from dynamodb");
         //const milliseconds = (new Date).getTime();
         var request = dynamodb.get({ TableName: tablename,
               Key: {"Id": job, "Type": "Job" }
@@ -94,8 +117,6 @@ var process_job_event = function(ts, user_name, message, callback) {
           });
         return request.promise().then(putS3);
     };
-
-    //var obj = message;
     function putS3(response) {
         // Job is success, put in S3
         const milliseconds = (new Date).getTime();
@@ -134,22 +155,6 @@ var process_job_event = function(ts, user_name, message, callback) {
         });
         return promise;
     };
-    /*
-    dynamodb.update(params, function(err, data) {
-        if (err) {
-          // NOTE: data is null, but apparently there is no error when a value is ""
-          // ValidationException: ExpressionAttributeValues contains invalid value:
-          // One or more parameter values were invalid: An AttributeValue may not contain an empty string for key :o
-          if (e == 'output')
-            log("failed to update dynamodb output field job Id=%s\n%s" %(job,message['value']));
-          else if (e == 'status')
-            log("failed to update dynamodb status fields job Id=%s\n%s" %(job,message['status']));
-          callback(new Error(`"${err.stack}"`));
-        } else {
-          publish_to_log_topic(message, callback);
-        }
-    });
-    */
     function handleError(error) {
       log(`handleError ${error.name}`);
       callback(new Error(`"${error.stack}"`), "Error")
@@ -159,17 +164,168 @@ var process_job_event = function(ts, user_name, message, callback) {
       callback(null, "Success");
     };
     var response = dynamodb.update(params);
-    if (status == 'success' || status == 'error') {
+    response.promise()
+      .then(handleDone)
+      .catch(handleError);
+}
+
+var process_job_event_status = function(ts, user_name, message, callback) {
+    // ts -- "Timestamp": "2018-05-10T01:47:26.794Z",
+    const log = require("debug")("foqus-sns-update.process_job_event_status")
+    const s3 = new AWS.S3();
+    const e = message['event'];
+    const status = message['status'];
+    const job = message['jobid'];
+    const msecs = Date.parse(ts);
+    const consumer = message['consumer'];
+    const session = message['sessionid']
+    const error_message = message['message'];
+    assert.strictEqual(e, "status");
+
+    log(`status=${status} username=${user_name},  job=${job}, session=${session}`);
+    publish_to_log_topic(e);
+    function getDynamoJob() {
+        // Job is success, put in S3
+        log("getDynamoJob: Get Job Description from dynamodb");
+        var request = dynamodb.get({ TableName: tablename,
+              Key: {"Id": job, "Type": "Job" }
+          });
+        return request.promise().then(putS3);
+    };
+    function putS3(response) {
+        // Job is success, put in S3
+        log("putS3: Put Job Description");
+        const milliseconds = (new Date).getTime();
+        var promise = new Promise(function(resolve, reject){
+            if (status=='success' || status=='error' || status=='submit' || status=='setup' || status=='running') {
+              response.Item.Output = response.Item.output;
+              response.Item.Session = response.Item.SessionId;
+              delete response.Item.output;
+              delete response.Item.SessionId;
+              response.Item.State = status;
+              if (status == 'error') {
+                log("error: " + error_message);
+                response.Item.Message = error_message;
+              }
+              var content = JSON.stringify(response.Item)
+              log("put3s: " + content);
+              if(content == undefined) {
+                  throw new Error("s3 object is undefined")
+              }
+              if(content.length == undefined) {
+                  throw new Error("s3 object is empty")
+              }
+              var key = `${user_name}/session/${session}/finished/${milliseconds}/${status}/${job}.json`;
+              if (status=='submit' || status=='setup' || status=='running') {
+                  key = `${user_name}/session/${session}/job/${job}/${status}.json`;
+              }
+              var params = {
+                Bucket: s3_bucket_name,
+                Key: key,
+                Body: content
+              };
+              log(`putS3(${params.Bucket}):  ${params.Key}`);
+              var request = s3.putObject(params);
+              resolve(request.promise());
+            } else {
+              log(`putS3 ignore state ${status}`);
+              resolve();
+            }
+        });
+        return promise;
+    };
+    function handleError(error) {
+      log(`Status handleError ${error.name}`);
+      callback(new Error(`"${error.stack}"`), "Error")
+    };
+    function handleDone() {
+      log("handleDone");
+      callback(null, "Success");
+    };
+
+    if (status == 'success' || status == 'error' || status == 'terminate' ) {
+        var params = {
+            TableName:tablename,
+            Key:{
+                "Id": job,
+                "Type":"Job"
+            },
+            UpdateExpression: "set #w=:w, #s=:s, ConsumerId=:c, #u=:u, #t=:t",
+            ExpressionAttributeValues:{
+                ":w":ts,
+                ":c":consumer,
+                ":u":user_name,
+                ":s":status,
+                ":t":Math.floor(Date.now()/1000 + 60*60*24)
+            },
+            ExpressionAttributeNames:{
+                "#w":"Finished",
+                "#u":"User",
+                "#s":"State",
+                "#t":"TTL"
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+        log(`finished job: job=${job}, status=${status}`);
+        log(JSON.stringify(params));
+        var response = dynamodb.update(params);
+        response.promise()
+          .then(getDynamoJob)
+          .then(handleDone)
+          .catch(handleError);
+        return;
+    }
+    if (status == 'expired') {
+        var key = `${user_name}/session/${session}/finished/${milliseconds}/expired/${job}.json`;
+        var item = {
+          Id: job,
+          Finished: ts,
+          Session: session,
+          Status: 'expired',
+          Message: 'Consumer failed to find job in database and marked it as expired',
+          Consumer: consumer
+        };
+        var content = JSON.stringify(item);
+        var params = {
+          Bucket: s3_bucket_name,
+          Key: key,
+          Body: content
+        };
+        log(`expired job: job=${job}, status=${status}`);
+        var request = s3.putObject(params);
+        response.promise()
+          .then(putS3)
+          .then(handleDone)
+          .catch(handleError);
+        return;
+    }
+    if (status == 'submit' | status == 'setup' | status == 'running') {
+      var params = {
+          TableName:tablename,
+          Key:{
+              "Id": job,
+              "Type":"Job",
+          },
+          UpdateExpression: "set #w=:s, #t=:t",
+          ExpressionAttributeValues:{
+              ":s":ts,
+              ":t":Math.floor(Date.now()/1000 + 60*60*1)
+          },
+          ExpressionAttributeNames:{
+              "#w":status,
+              "#t":'TTL'
+          },
+          ReturnValues:"UPDATED_NEW"
+      };
+      log(`active job: job=${job}, status=${status}`);
+      var response = dynamodb.update(params);
       response.promise()
         .then(getDynamoJob)
         .then(handleDone)
         .catch(handleError);
-
-    } else {
-      response.promise()
-        .then(handleDone)
-        .catch(handleError);
+      return;
     }
+    assert.fail(`"process_job_event_status, unexpected status ${status}"`);
 }
 
 var publish_to_log_topic = function(message) {
@@ -185,8 +341,7 @@ var publish_to_log_topic = function(message) {
           Message: JSON.stringify(message),
           TopicArn:  topic_arn
         };
-        log("SNS Publish: " + topic_arn);
-        log(params);
+        log(`publish_to_log_topic ${params.Message}`);
         sns.publish(params, function(err, data) {
         });
     });
@@ -197,7 +352,7 @@ var process_consumer_event = function(ts, message, callback) {
     var e = message['event'];
     //var status = message['status'];
     var consumer = message['consumer'];
-    var msecs = Date.parse(ts);
+    const msecs = Date.parse(ts);
     var instance_id = message['instanceid'];
     var update_expr = "set " + e + " = :s, #t=:t";;
     var expr_attr_vals = {":s":ts, ":t":Math.floor(Date.now()/1000 + 60*60*1)};
@@ -235,7 +390,9 @@ var process_consumer_event = function(ts, message, callback) {
       }
     });
 }
-
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 /*
  * exports.handler
  * Listens to SNS Update Topic, event parameter specifies type of
@@ -246,25 +403,49 @@ var process_consumer_event = function(ts, message, callback) {
  *
  */
 exports.handler = function(event, context, callback) {
-    //log('Received event:', JSON.stringify(event, null, 4));
-    var message = JSON.parse(event.Records[0].Sns.Message);
-    const attrs = event.Records[0].Sns.MessageAttributes;
-    const ts = event.Records[0].Sns.Timestamp;
-    log('Received event:', event.Records[0].Sns.Message);
-    if (util.isArray(message) == false) {
-        message.resource = "job";
-        message.event = "submit"
-        message = [message];
-    }
-    for (var i = 0; i < message.length; i++) {
-      var resource = message[i]['resource'];
-      if (resource == "job") {
-          const user_name = attrs.username.Value;
-          process_job_event(ts, user_name, message[i], callback);
-      } else if (resource == "consumer") {
-          process_consumer_event(ts, message[i], callback);
-      } else {
-        log("WARNING: NotImplemented skip update resource=" + resource);
+    for (var j = 0; j < event.Records.length; j++) {
+      var message = JSON.parse(event.Records[j].Sns.Message);
+      var attrs = event.Records[j].Sns.MessageAttributes;
+      var ts = event.Records[j].Sns.Timestamp;
+      log('Received event:', event.Records[j].Sns.Message);
+
+      if (util.isArray(message) == false) {
+          message = [message];
+      }
+
+      for (var i = 0; i < message.length; i++) {
+          log(`MESSAGE ${j}.${i}: ${JSON.stringify(message[i])}`);
+          //await sleep(2000);
+          var resource = message[i]['resource'];
+          var e = message[i]['event'];
+          if (resource == "job") {
+              var user_name = attrs.username.Value;
+              if (e == "output") {
+                  process_job_event_output(ts, user_name, message[i], callback);
+              } else if (e == "status") {
+                  process_job_event_status(ts, user_name, message[i], callback);
+              } else if (e == "submit") {
+                  log(`submit message for job ${message[i].jobid}`)
+                  process_job_event_status(ts, user_name, message[i], callback);
+              } else {
+                assert.fail(`"Job with unknown event ${e}"`);
+              }
+          } else if (resource == "consumer") {
+              process_consumer_event(ts, message[i], callback);
+          } else if (attrs.event && attrs.event.Value.startsWith('session.')) {
+              const a = attrs.event.Value.split('.');
+              const session_id = a[2];
+              const state = a[1];
+              log(`"session ${session_id}:  event ${state}"`)
+              if (state == "terminate")
+                process_session_event_terminate(ts, session_id, callback);
+              else
+                log(`"WARNING: Not Implemented Session ${attrs.event.Value}"`)
+          }
+          else {
+            log("WARNING: NotImplemented skip update resource=" + resource);
+          }
       }
     }
+    log('Finished');
 };
--- cloud/aws/node/post-session-append/index.js
+++ cloud/aws/node/post-session-append/index.js
@@ -83,7 +83,7 @@ exports.handler = function(event, context, callback) {
             }
             var params = {
               Bucket: s3_bucket_name,
-              Key: user_name + '/' + session_id + '/' + milliseconds + '.json',
+              Key: user_name + '/session/create/' + session_id + '/' + milliseconds + '.json',
               Body: content
             };
             log(`putS3(${params.Bucket}):  ${params.Key}`);
@@ -106,12 +106,14 @@ exports.handler = function(event, context, callback) {
           id_list.push(obj[i].Id);
           item = {Id: obj[i].Id,
                   Type: "Job",
+                  State: "create",
                   Create: d.toISOString(),
                   SessionId: session_id,
                   User: user_name,
                   Initialize: obj[i].Initialize,
                   Input: obj[i].Input,
                   Reset:obj[i].Reset,
+                  TTL: Math.floor(Date.now()/1000 + 60*60*12),
                   Simulation: obj[i].Simulation,
                   Application: "foqus"};
 

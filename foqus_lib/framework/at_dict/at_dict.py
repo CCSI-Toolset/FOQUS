6dfcbc3ee74d8db788139b34fee867ffc151b7d3
Keith Beattie <KSBeattie@lbl.gov>
2019-10-28 22:25:18 -0700

    pylint find: addresses #600

--- foqus_lib/framework/at_dict/at_dict.py
+++ foqus_lib/framework/at_dict/at_dict.py
@@ -1,29 +1,30 @@
 
 class AtDict(dict):
     def __getattr__(self, name):
-            return self[name]
+        return self[name]
 
     def __setattr__(self, name, value):
         if name in list(self.keys()):
             self[name] = value
         else:
-             super(AtDict, self).__setattr__(name, value)
+            super().__setattr__(name, value)
+
 
 class OrderedAtDict(AtDict):
     def __init__(self, *args, **kwargs):
         self._ordered_keys = []
-        super(AtDict, self).__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
 
     def __setitem__(self, name, value):
         if name not in self._ordered_keys:
             self._ordered_keys.append(name)
-        super(AtDict, self).__setitem__(name, value)
+        super().__setitem__(name, value)
 
     def __delitem__(self, name):
         if name not in self._ordered_keys:
             return
         self._ordered_keys.remove(name)
-        super(AtDict, self).__delitem__(name)
+        super().__delitem__(name)
 
     def __iter__(self):
         for k in self._ordered_keys:

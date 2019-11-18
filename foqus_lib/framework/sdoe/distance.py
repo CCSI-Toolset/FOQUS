21e33932848c1c0b8aa962507334010e91209b24
ngbrenda <ng30@llnl.gov>
2019-11-17 17:51:42 -0800

    Fixed compute_dist()

--- foqus_lib/framework/sdoe/distance.py
+++ foqus_lib/framework/sdoe/distance.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def compute_dist(mat,      # numpy array of shape (N, ncols) and type 'float'
                  scl=None, # [usf] numpy array of shape (ncols,) and type 'float'
-                 wt=None,  # [nusf] numpy array of shape (_,) and type 'float'
+                 wt=None,  # [nusf] numpy array of shape (N,) and type 'float'
                  hist=[]):
     if hist:
         mat = np.concatenate((mat, hist), axis=0)
@@ -10,17 +10,17 @@ def compute_dist(mat,      # numpy array of shape (N, ncols) and type 'float'
     N, ncols = mat.shape
     dmat = np.full((N, N), np.nan)
 
-    if scl:
+    if scl is not None:
         assert scl.shape[0] == ncols, 'SCL should be of dim %d.' % ncols
         mat = mat / np.repeat(np.reshape(scl, (1, ncols)), N, axis=0)
         val = 10
         
     for i in range(N):
         x = np.repeat(np.reshape(mat[i,:], (1, ncols)), N, axis=0) - mat
-        dmat[:, i] = np.sum(np.square(x), axis=1)
+        dmat[:,i] = np.sum(np.square(x), axis=1)
 
-    if wt:
-        dmat = np.multiply(mat, np.outer(wt, wt))
+    if wt is not None:
+        dmat = np.multiply(dmat, np.outer(wt, wt))
         val = 9999 
 
     np.fill_diagonal(dmat, val)

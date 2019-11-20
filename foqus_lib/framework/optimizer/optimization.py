a58b2c5c0e32d52a74dec21d0f0516e381f211cd
Anuja Deshpande <anuja.deshpande@netl.doe.gov>
2019-11-19 21:12:32 -0500

    removing commented code lines

--- foqus_lib/framework/optimizer/optimization.py
+++ foqus_lib/framework/optimizer/optimization.py
@@ -78,6 +78,3 @@ class optimization(threading.Thread):
         except Exception as e:
             logging.getLogger("foqus." + __name__).exception(
                 "Exception in optimization thread")
-#            self.ex = sys.exc_info()
-#            self.msgQueue.put(traceback.format_exception(
-#                self.ex[0], self.ex[1], self.ex[2]))

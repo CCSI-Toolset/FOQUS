a105d8131a91ae03a8c209ee495b17f4af717f53
Pedro Sotorrio <sotorrio1@llnl.gov>
2019-12-19 09:19:57 -0800

    fixing plotting

--- foqus_lib/gui/sdoe/sdoeAnalysisDialog.py
+++ foqus_lib/gui/sdoe/sdoeAnalysisDialog.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import configparser
 
 from foqus_lib.framework.sdoe import order, sdoe
+from foqus_lib.framework.sdoe.df_utils import load
 from .sdoeSetupFrame import *
 from .sdoePreview import sdoePreview
 
@@ -525,7 +526,7 @@ class sdoeAnalysisDialog(_sdoeAnalysisDialog, _sdoeAnalysisDialogUI):
         count = 0
         for mwr in mwr_list:
             self.analysis.append([mwr, results[mwr]['design_size'], results[mwr]['num_restarts'], results[mwr]['elapsed_time'], fnames[mwr],
-                                  config_file, results[mwr]['best_val']])
+                                  config_file, results[mwr]['best_val'], results[mwr]['best_cand']])
 
             self.updateAnalysisTable()
             self.designInfoNUSF_dynamic.setText('mwr = %d, n = %d' % (mwr, results[mwr]['num_restarts']))
@@ -756,6 +757,9 @@ class sdoeAnalysisDialog(_sdoeAnalysisDialog, _sdoeAnalysisDialogUI):
         config = configparser.ConfigParser(allow_no_value=True)
         config.read(config_file)
         hfile = config['INPUT']['history_file']
+        cfile = config['INPUT']['candidate_file']
+        include = [s.strip() for s in config['INPUT']['include'].split(',')]
+        types = [s.strip() for s in config['INPUT']['type'].split(',')]
 
         if hfile == '':
             hname = None
@@ -763,7 +767,11 @@ class sdoeAnalysisDialog(_sdoeAnalysisDialog, _sdoeAnalysisDialogUI):
             hname = hfile
         sdoeData = LocalExecutionModule.readSampleFromCsvFile(fullName, False)
         if self.type == 'NUSF':
-            nusf = {'dmat':self.analysis[row][4]['dmat']}
+            cand = load(cfile)
+            i = types.index('Weight')
+            wcol = include[i]  # weight column name
+            wts = cand[wcol]
+            nusf = {'cfile': cfile, 'wts': wts, 'des':self.analysis[row][7]}
         else:
             nusf = None
         dialog = sdoePreview(sdoeData, hname, dirname, nusf, self)

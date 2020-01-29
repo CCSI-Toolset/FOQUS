69fb71f81d3c472e45f3fdb8ea33ca036003c718
Joshua Boverhof <jrboverhof@lbl.gov>
2020-01-29 01:53:17 +0000

    Empty recycle bin

--- cloud/aws/Seal-VM.ps1
+++ cloud/aws/Seal-VM.ps1
@@ -8,6 +8,7 @@ Stop-Service -DisplayName  'Turbine Web API Service'
 Remove-Item -Recurse -Path C:\ProgramData\foqus_service -ErrorAction SilentlyContinue
 Remove-Item -Path 'C:\Program Files (x86)\Turbine\Lite\Data\TurbineCompactDatabase.sdf' -ErrorAction SilentlyContinue
 Remove-Item -Path 'C:\Program Files (x86)\Turbine\Lite\Logs\*.log' -Recurse -ErrorAction SilentlyContinue
+Clear-RecycleBin -Force
 
 #Start-Service -DisplayName  'Turbine Web API Service'
 #Start-Service -DisplayName 'FOQUS Cloud Service'

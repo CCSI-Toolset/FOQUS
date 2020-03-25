5511b189365339c25232a1e2cadd618b4992a9d6
Keith Beattie <KSBeattie@lbl.gov>
2020-03-25 15:40:50 -0700

    somehow missed boto3

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ dist = setup(
     # requirements.txt should stay empty (other than the "-e .")
     install_requires=[
         "adodbapi>=2.6.0.7; sys_platform == 'win32'",
+        "boto3",
         "cma",
         "matplotlib",
         "mlrose",

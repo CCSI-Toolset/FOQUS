63e291ee74e18ba921f3f62c8008e8559232a84d
Keith Beattie <ksbeattie@lbl.gov>
2020-06-29 00:03:01 -0700

    Update make_foqus_icon.py
    This one works, though it still be better in setup.py

--- make_foqus_icon.py
+++ make_foqus_icon.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
+""" Create a windows shortcut on the desktop to start FOQUS """
 import os
 import pathlib as pl
-
+import sys
 import winshell
 
 # Define all the file paths needed for the shortcut
 desktop = pl.Path(winshell.desktop())
+link_filepath = desktop / "ccsi-foqus.lnk"
 conda_base = pl.Path(os.environ['CONDA_PREFIX_1'])
+activate_bat = conda_base / 'Scripts' / 'activate.bat'
 conda_env = pl.Path(os.environ['CONDA_PREFIX'])
-win32_cmd = str(pl.Path(winshell.folder('CSIDL_SYSTEM')) / 'cmd.exe')
-icon = str(conda_base / "Menu" / "Iconleak-Atrous-Console.ico")
-
-# This will point to My Documents/py_work. Adjust to your preferences
-my_working = str(pl.Path(winshell.folder('PERSONAL')) / "ccsi-foqus")
-link_filepath = str(desktop / "ccsi-foqus.lnk")
+foqus_exe = conda_env / 'Scripts' /'foqus.exe'
+win32_cmd = pl.Path(winshell.folder('CSIDL_SYSTEM')) / 'cmd.exe'
+this_dir = pl.Path(__file__).resolve().parent
+icon = this_dir / "foqus.ico"
+working_dir = pl.Path(winshell.folder('PERSONAL'))  # "Documents"
 
 # Build up all the arguments to cmd.exe
-# Use /K so that the command prompt will stay open
-arg_str = "/K " + str(conda_base / "Scripts" / "activate.bat") + " " + str(conda_env)
+cmd_args = f"/K {activate_bat} {conda_env} & {foqus_exe} && exit"
+
+if link_filepath.exists():
+    print(f'Overwriting shortcut: {link_filepath}')
 
 # Create the shortcut on the desktop
-with winshell.shortcut(link_filepath) as link:
-    link.path = win32_cmd
+with winshell.shortcut(str(link_filepath)) as link:
+    link.path = str(win32_cmd)
     link.description = "CCSI FOQUS"
-    link.arguments = arg_str
-    link.icon_location = (icon, 0)
-    link.working_directory = my_working
+    link.arguments = cmd_args
+    link.icon_location = (str(icon), 0)
+    link.working_directory = str(working_dir)

Fixing reverse scaling for NUSF and IRSF.

foqus_lib/framework/sdoe/irsf.py
@@ -25,9 +25,13 @@ def criterion(cand, args, nr, nd, mode='maximin', hist=None, test=False):
     inp_x = cand[args['idx']]
     xcols = list(inp_x.columns)
     norm_x = unitscale_cand(inp_x)
+    xmin = inp_x.to_numpy().min(axis=0)
+    xmax = inp_x.to_numpy().max(axis=0)
     inp_y = cand[args['idy']]
     ycols = list(inp_y.columns)
     norm_y = unitscale_cand(inp_y)
+    ymin = inp_y.to_numpy().min(axis=0)
+    ymax = inp_y.to_numpy().max(axis=0)
 
     # if testing, T1 is for X only search, and T2 for PF search with 0.5 weight
     if test:
@@ -92,6 +96,10 @@ def criterion(cand, args, nr, nd, mode='maximin', hist=None, test=False):
                                                                                            args['max_iterations'],
                                                                                            nr,
                                                                                            mode)
+
+        # Reverse scaling
+        PFxdes[i] = Inv_scale_cand(PFxdes[i], xmin, xmax)
+        PFydes[i] = Inv_scale_cand(PFydes[i], ymin, ymax)
 
     # Pareto Front for each weight i is created here. For example if 3 Pareto solutions were found for ith weight, and
     # the design size is 10, then PFxdes will contain all 3 designs (10 each) in an order, so the number of rows in
@@ -163,10 +171,9 @@ def unitscale_cand(cand):
 
 def Inv_scale_cand(cand, xmin, xmax):
     cand_arr = np.asarray(cand)
-    inv_norm_cand = np.zeros((len(cand_arr), len(cand_arr.T)))
-    for i in range(cand_arr.shape[1] - 1):
+    inv_norm_cand = np.zeros(cand_arr.shape)
+    for i in range(cand_arr.shape[1]):
         inv_norm_cand[:, i] = ((cand_arr[:, i] + 1) / 2) * (xmax[i] - xmin[i]) + xmin[i]
-    inv_norm_cand[:, -1] = cand_arr[:, -1]
     return inv_norm_cand
 
 
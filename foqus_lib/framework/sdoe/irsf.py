a24b1ece9da830ac555a1c1af63d6a951bf2a6e8
Pedro Sotorrio <sotorrio1@llnl.gov>
2021-08-05 12:22:32 -0700

    fixing small bug

--- foqus_lib/framework/sdoe/irsf.py
+++ foqus_lib/framework/sdoe/irsf.py
@@ -165,7 +165,7 @@ def Inv_scale_cand(cand, xmin, xmax):
     cand_arr = np.asarray(cand)
     inv_norm_cand = np.zeros((len(cand_arr), len(cand_arr.T)))
     for i in range(cand_arr.shape[1] - 1):
-        inv_norm_cand[:, i] = ((cand_arr[:, i] + 1) / 2) * (xmax[i] - xmin[i]) + xmin[i]
+        inv_norm_cand[:, i] = (cand_arr[:, i] * (xmax[i] - xmin[i])) + xmin[i]
     inv_norm_cand[:, -1] = cand_arr[:, -1]
     return inv_norm_cand
 

9cfc86c5b7244deccaa8da19dcd014d683b98f4c
Ludovico Bianchi <lbianchi@lbl.gov>
2021-09-07 19:15:18 -0700

    Try taking debug snapshot manually before problematic call

--- foqus_lib/gui/tests/test_uq.py
+++ foqus_lib/gui/tests/test_uq.py
@@ -74,6 +74,7 @@ class TestUQ(_HasAttributesSetByFixture):
     @pytest.fixture(scope='class')
     def run_simulation(self, qtbot):
         with qtbot.focusing_on(self.frame.simulationTable):
+            qtbot.take_debug_snapshot(label='run-simulation-1')
             qtbot.select_row(0)
             # qtbot.wait(5_000)
             with qtbot.intercepting_modal(), qtbot.wait_signal(self.frame.runsFinishedSignal, timeout=90_000):

8ead8fe29852acae5d9a827b4104570076c8c585
Ludovico Bianchi <lbianchi@lbl.gov>
2021-09-07 19:23:52 -0700

    Try taking snapshot before looking into table

--- foqus_lib/gui/tests/test_uq.py
+++ foqus_lib/gui/tests/test_uq.py
@@ -73,11 +73,11 @@ class TestUQ(_HasAttributesSetByFixture):
 
     @pytest.fixture(scope='class')
     def run_simulation(self, qtbot):
+        qtbot.take_debug_snapshot(label='run-simulation-1')
         with qtbot.focusing_on(self.frame.simulationTable):
             qtbot.select_row(0)
             # qtbot.wait(5_000)
             with qtbot.intercepting_modal(), qtbot.wait_signal(self.frame.runsFinishedSignal, timeout=90_000):
-                qtbot.take_debug_snapshot(label='run-simulation-1')
                 qtbot.using(column='Launch').click()
 
     @pytest.mark.usefixtures('run_simulation')

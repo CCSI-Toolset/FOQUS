d7a7392c62a0547c59f1abde3f0880ce7570f6db
Ludovico Bianchi <lbianchi@lbl.gov>
2023-03-07 14:57:03 -0600

    Move files to correct directory and format with Black

--- foqus_lib/framework/sdoe/test/test_distance.py
+++ foqus_lib/framework/sdoe/test/test_distance.py
@@ -2,32 +2,28 @@ from unittest import result
 import numpy as np
 from foqus_lib.framework.sdoe import distance
 
+
 def test_compute_dist():
     # let N = 4, nx = 2, M = 3
-    
-    mat = np.array([[1.5, 2.5],
-                    [3.5, 4.5],
-                    [1.0, 2.0], 
-                    [3.0, 4.0]])
+
+    mat = np.array([[1.5, 2.5], [3.5, 4.5], [1.0, 2.0], [3.0, 4.0]])
     print(mat.shape)
     scl = np.array([2.2, 3.2])
     wt = np.array([1.6, 2.6, 3.6, 4.6])
-    hist_xs = np.array([[1.7, 2.7], 
-                        [3.7, 2.3], 
-                        [3.3, 4.3]])
+    hist_xs = np.array([[1.7, 2.7], [3.7, 2.3], [3.3, 4.3]])
     hist_wt = np.array([10.0, 11.0, 12.0])
-    
-    mat_and_hist_xs = distance.compute_dist(mat, scl=None, wt=None, hist_xs=hist_xs, hist_wt=None)
+
+    mat_and_hist_xs = distance.compute_dist(
+        mat, scl=None, wt=None, hist_xs=hist_xs, hist_wt=None
+    )
     assert isinstance(mat_and_hist_xs, np.ndarray)
     assert mat_and_hist_xs.shape == (7, 7)
     # expect_mat_and_hist_xs = np.array([[1.5, 2.5],
     #                                 [3.5, 4.5],
-    #                                 [1.0, 2.0], 
+    #                                 [1.0, 2.0],
     #                                 [3.0, 4.0],
-    #                                 [1.7, 2.7], 
-    #                                 [3.7, 2.3], 
+    #                                 [1.7, 2.7],
+    #                                 [3.7, 2.3],
     #                                 [3.3, 4.3]])
-    
-    # assert np.array_equal(mat_and_hist_xs, expect_mat_and_hist_xs)
 
-    
+    # assert np.array_equal(mat_and_hist_xs, expect_mat_and_hist_xs)
--- foqus_lib/framework/sdoe/test/test_nusf.py
+++ foqus_lib/framework/sdoe/test/test_nusf.py
@@ -3,16 +3,14 @@ import numpy as np
 import pandas as pd
 from foqus_lib.framework.sdoe import nusf
 
+
 def test_criterion():
-    
-    cand = pd.DataFrame([(1, 1, 1),
-    (2, 2, 1),
-    (3, 3, 1),
-    (4, 4, 1)])
+
+    cand = pd.DataFrame([(1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 4, 1)])
     args = {
         "icol": "",
         "xcols": [0, 1],
-        "scale_factors": pd.Series(1., index=range(len(cand))),
+        "scale_factors": pd.Series(1.0, index=range(len(cand))),
         "wcol": 2,
         "max_iterations": 100,
         "mwr_values": [1, 2, 4, 8, 16],
@@ -20,10 +18,10 @@ def test_criterion():
     }
     nr = 3
     nd = 2
-    mode="maximin"
+    mode = "maximin"
     hist = None
-    
-    result = nusf.criterion(cand=cand, args=args,nr=nr, nd=nd, mode=mode, hist=None)
-    
+
+    result = nusf.criterion(cand=cand, args=args, nr=nr, nd=nd, mode=mode, hist=None)
+
     for mwr in args["mwr_values"]:
         assert result[mwr].get("best_cand") is not None

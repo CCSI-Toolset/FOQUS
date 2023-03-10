e2402d7214a39f46d350109aa65862ae28c81923
Keith Beattie <KSBeattie@lbl.gov>
2023-03-09 16:40:51 -0800

    Adding addheader to requirements-dev.txt Fixing years in docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "FOQUS"
-copyright = "2012 - 2022 by CCSI Team All rights reserved."
+copyright = "2012 - 2023 by CCSI Team All rights reserved."
 author = "CCSI"
 
 # The version info for the project you're documenting, acts as replacement for

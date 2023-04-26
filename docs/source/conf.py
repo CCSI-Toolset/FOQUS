7f5755dacb265a09db6f13967575e79a25d9cb0e
Ludovico Bianchi <lbianchi@lbl.gov>
2023-04-26 10:58:07 -0500

    Update intersphinx_mapping to non-deprecated format (#1140)
    Co-authored-by: Ludovico Bianchi <me@ludob.com>
    (cherry picked from commit 7553a9da1003859d9def22a4357a8972bd04f31b)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -379,7 +379,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 rst_epilog = """
 .. |examples_reminder_text| replace:: The **examples/** directory refers to the location where the FOQUS examples were installed, as described in :ref:`install_examples`.

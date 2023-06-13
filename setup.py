04d6e7c06958517f5cbed3c489ee1237b2c86b7e
kbuma <kberket@lbl.gov>
2023-06-13 19:24:51 -0400

    fixed logic for conditional install of pyQt5

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ dist = setup(
         "numpy",
         "pandas",
         "psutil",
-        "PyQt5==5.15.7; platform_machine != 'arm64' and sys_platform == 'darwin'",
+        "PyQt5==5.15.7; platform_machine != 'arm64' or sys_platform != 'darwin'",
         "pywin32<305; sys_platform == 'win32'",
         "requests",
         "scipy",

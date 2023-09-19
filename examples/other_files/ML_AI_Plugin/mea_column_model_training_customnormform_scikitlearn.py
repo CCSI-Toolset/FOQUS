394c69cddf0fca6d524ecf06f8216c12208359d7
Brandon Paul <bpaul@keylogic.com>
2023-09-19 12:19:40 -0700

    Fix comment

--- examples/other_files/ML_AI_Plugin/mea_column_model_training_customnormform_scikitlearn.py
+++ examples/other_files/ML_AI_Plugin/mea_column_model_training_customnormform_scikitlearn.py
@@ -79,7 +79,7 @@ for i in range(len(xdata)):
 
 model_data = np.concatenate(
     (xdata, zdata), axis=1
-)  # PyTorch requires a Numpy array as input
+)  # SciKit Learn requires a Numpy array as input
 
 # define x and z data, not used but will add to variable dictionary
 xdata = model_data[:, :-2]

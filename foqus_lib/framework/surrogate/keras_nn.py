8dd598a24c07de6ec31acc6c140be8ac45cfea1e
Ludovico Bianchi <lbianchi@lbl.gov>
2023-09-20 20:32:43 -0500

    Remove f-string syntax incompatible with Python 3.7

--- foqus_lib/framework/surrogate/keras_nn.py
+++ foqus_lib/framework/surrogate/keras_nn.py
@@ -361,11 +361,11 @@ class surrogateMethod(surrogate):
 
         epochs_count = self.options["epoch"].value
         verbose_setting = self.options["verbose"].value
-        self.msgQueue.put(f"{self.input=}")
-        self.msgQueue.put(f"{self.output=}")
+        self.msgQueue.put(f"input vars: {self.input}")
+        self.msgQueue.put(f"output vars: {self.output}")
         input_data, output_data = self.getSelectedInputOutputData()
-        self.msgQueue.put(f"{input_data.columns=}")
-        self.msgQueue.put(f"{output_data.columns=}")
+        self.msgQueue.put(f"input data columns: {input_data.columns}")
+        self.msgQueue.put(f"output data columns: {output_data.columns}")
 
         # np.random.seed(46)
         # rn.seed(1342)

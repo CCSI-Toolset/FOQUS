b778e7f668574dca1c821f2b47d83f86e5959255
Karlo Berket <kberket@lbl.gov>
2024-09-24 13:22:32 -0700

    update docs for SimSinter and TurbineLite configuration to clarify path locations (#1238)
    * docs for SimSinter and TurbineLite configuration provide most likely path
    
    * fixing code block
    
    * fixing spelling
    
    * fix extra quote

--- foqus_lib/framework/surrogate/BSS-ANOVA.py
+++ foqus_lib/framework/surrogate/BSS-ANOVA.py
@@ -18,7 +18,7 @@ Surrogate plugins need to have the string "#FOQUS_SURROGATE_PLUGIN" near the
 beginning of the file (see pluginSearch.plugins() for exact character count of
 text).  They also need to have a .py extension and inherit the surrogate class.
 
-* Plugin wrapper for the BSS-ANOVA surrogate model builer.
+* Plugin wrapper for the BSS-ANOVA surrogate model builder.
 * BSS-ANOVA is executed in R and a working R install with the quadprog
   package is required.  The user must install R
 * BSS-ANOVA Ref:

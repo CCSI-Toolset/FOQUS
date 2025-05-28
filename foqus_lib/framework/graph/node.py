76e7805840627138026ec8a1474f0013cb75679c
Keith Beattie <KSBeattie@lbl.gov>
2025-05-27 18:45:45 -0700

    new typo

--- foqus_lib/framework/graph/node.py
+++ foqus_lib/framework/graph/node.py
@@ -293,7 +293,7 @@ class NodeEx(foqusException):
         self.codeString[23] = "Could not convert numpy value to list"
         self.codeString[27] = "Can't read variable in results (see log)"
         self.codeString[50] = "Node script interrupt exception"
-        self.codeString[61] = "Unknow type string"
+        self.codeString[61] = "Unknown type string"
         self.codeString[self.ERROR_CONFIGURATION_MISSING] = (
             "Model Missing Configuration"
         )

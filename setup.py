49bf39947224cf8d4c94d8b384c1dabd18edde28
Keith Beattie <KSBeattie@lbl.gov>
2026-02-24 13:22:42 -0800

    restore matplotlib version restrictions

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ dist = setup(
         # prebuilt wheels not available on 3.11 for matplotlib<3.6 on: macos arm64, win32
         # matplotlib>=3.8 has API incompatibilities with SDoE plots
         # see CCSI-Toolset/FOQUS#1211 for more information
-        "matplotlib",
+        "matplotlib >= 3.6.0, <= 3.7.5",
         "python-tsp==0.3.1",
         "mplcursors",
         "numpy",
